22b79a8243d7885996d4404685e3fca722c9e814
yangmengjiao <mengjiaoshun@gmail.com>
2018-04-04 15:13:54 -0500

    Initial commit

---
+++ app/src/main/java/edu/mum/lab1_1/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.mum.lab1_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn1;
+    private EditText et1;
+    private EditText et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
+
+        et1 = findViewById(R.id.input);
+        et2 = findViewById(R.id.output);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String et1Text = et1.getText().toString();
+        if(!TextUtils.isEmpty(et1Text)){
+           // String et1Text = et1.getText().toString();
+            double lbs = Double.parseDouble(et1Text);
+
+            double kgs = 0.4532*lbs;
+            et2.setText(kgs+"");
+        }else{
+            return;
+        }
+
+    }
+}
---
+++ app/src/main/java/edu/mum/lab1_2/Main2Activity.java
@@ -0,0 +1,56 @@
+package edu.mum.lab1_2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+import edu.mum.R;
+
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn1;
+    private Button btn2;
+    private EditText et1;
+    ArrayList<String> array = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btn1=findViewById(R.id.button1);
+        btn1.setOnClickListener(this);
+        btn2=findViewById(R.id.button2);
+        btn2.setOnClickListener(this);
+        et1 = findViewById(R.id.input);
+
+        array.add("Hamburger");
+        array.add("Pizza");
+        array.add("Mexican");
+        array.add("American");
+        array.add("Chinese");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.button1:
+
+                break;
+            case  R.id.button2:
+                break;
+                default:
+                    break;
+
+        }
+    }
+
+
+}
